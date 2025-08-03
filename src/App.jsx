cc773d792a4f92af9518cd9c57941be7045dcdda
Boby Keshri <bobyk25122000>
2025-08-03 13:03:44 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar";
+const App = () => {
+
+    return (
+        <div>
+            <Navbar />
+            <hr />
+
+            <h1>I am App Component</h1>
+            <h2>Hello World</h2>
+        </div>
+    )
+
+};
+
+export default App;
